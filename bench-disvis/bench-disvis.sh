40ad17edd3b7436a54d8a8838ddc3ea07436ec7d
mariojmdavid <mariojmdavid@gmail.com>
2015-12-23 13:25:18 +0000

    Add scripts and input files for disvis

---
+++ bench-disvis/bench-disvis.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# bench-disvis.sh: benchmark of disvis run on GPUs
+# Execution on physical hosts or VMs
+#
+# Copyright (C) LIP and IndigoDataCloud EU project
+# Author: Mario David <david@lip.pt>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+WDIR=`pwd`
+INPUT_DIR=${WDIR}/"PRE5-PUP2-complex"
+TIME="/usr/bin/time"
+TIME_STR="time = %e sec\n Mem = %M kB"
+PDB1=${INPUT_DIR}/O14250.pdb
+PDB2=${INPUT_DIR}/Q9UT97.pdb
+REST=${INPUT_DIR}/restraints.dat
+
+
+TIME_RES="tr.txt"
+OUT_DIR=${WDIR}/"res"
+DISVIS_PAR="-a 9.72 -vs 2"
+
+#TYPE is either -g for GPU or -p NN for CPU NN processes
+TYPE="-g"
+
+${TIME} -f ${TIME_STR} -o ${TIME_RES} \
+    disvis ${PDB1} ${PDB2} ${REST} ${TYPE} -d ${OUT_DIR}
+
+
