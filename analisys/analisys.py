d57670f3a7d4288785ca755ee1c747eb518b7299
mariojmdavid <mariojmdavid@gmail.com>
2015-12-27 22:05:15 +0000

    script new version

--- analisys/analisys.py
+++ analisys/analisys.py
@@ -5,6 +5,7 @@
 #########################################################################
 
 import os
+import pprint
 import numpy
 
 if __name__ == '__main__':
@@ -17,15 +18,21 @@ if __name__ == '__main__':
     angles = ['5.0', '10.0']
     voxspac = ['1', '2']
     types = ['GPU', 'CPU']
-    lruns = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+    lrunsG = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+    lrunsC = ['1', '2']
+    ljsres = []
 
+    i = 0
     for mach in machines:
+        lncores = [x for x in range(tot_cpus[i], 0, -2)]
+        lncores.append(1)
+        print lncores
         for cs in cases:
             for ang in angles:
                 for vs in voxspac:
                     jsres = {'machine': mach, 'case': cs, 'angle': ang,
-                             'voxspac': vs, 'rtime': []}
-                    for n in lruns:
+                             'voxspac': vs, 'type': 'GPU', 'rtime': []}
+                    for n in lrunsG:
                         tr_file = root_dir + os.sep + mach + os.sep + \
                             'time-' + cs + os.sep + 'tr_ang-' + ang + \
                             '-vs-' + vs + '-type-GPU' + '-n-' + n + '.txt'
@@ -37,5 +44,26 @@ if __name__ == '__main__':
                             jsres['rtime'].append(rtime)
                         except IOError:
                             print 'Cannot open', tr_file
+                    ljsres.append(jsres)
+                    for ncores in lncores:
+                        jsres = {'machine': mach, 'case': cs, 'angle': ang,
+                                 'voxspac': vs, 'type': 'CPU',
+                                 'ncores': ncores, 'rtime': []}
+                        for n in lrunsC:
+                            tr_file = root_dir + os.sep + mach + os.sep + \
+                                'time-' + cs + os.sep + 'tr_ang-' + ang + \
+                                '-vs-' + vs + '-type-CPU' + '-ncores-' + \
+                                str(ncores) + '-n-' + n + '.txt'
+                            try:
+                                f = open(tr_file)
+                                #print '-> File: %s' % tr_file
+                                s = f.readline()
+                                rtime = s.split('=')[1].split()[0]
+                                jsres['rtime'].append(rtime)
+                            except IOError:
+                                print 'Cannot open', tr_file
+                            ljsres.append(jsres)
 
-    print jsres
+        i += 1
+
+    pprint.pprint(ljsres)
