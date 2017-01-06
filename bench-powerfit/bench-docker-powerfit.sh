edf54cd63ef3be855edd754afbf609c23d016b1d
mariojmdavid <mariojmdavid@gmail.com>
2017-01-06 21:45:53 +0000

    add scripts for powerfit

--- bench-powerfit/bench-docker-powerfit.sh
+++ bench-powerfit/bench-docker-powerfit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# bench-powerfit.sh: benchmark of powerfit run on GPUs
+# bench-disvis.sh: benchmark of disvis run on GPUs
 # Execution on physical hosts or VMs
 #
 # Copyright (C) LIP and IndigoDataCloud EU project
@@ -20,21 +20,27 @@
 # limitations under the License.
 #
 
-# This is for physical machines or VMs
-MACH=Phys-Mach
+# Docker container name
+DOCK_NAME=powerfit-ubuntu16.04-build1
 
 # Variables to change by the user, should turn into argument to the script
 CASE="GroEL-GroES"
 #CASE="RsgA-ribosome"
 
 # Number of runs of each type for statistical purposes
-NRUNS=3
+NRUNS=1
 
 #############################################################
 # From here on everyhting is fixed
-WDIR=`pwd`
+LWDIR=`pwd`   # Local/physical directory
+WDIR='/home'  # Working dir inside the docker
 TIME="/usr/bin/time"
 TIME_STR="%e\ntime = %e sec\nMem = %M kB"
+DOCK_NVD="--device=/dev/nvidia0:/dev/nvidia0 \
+          --device=/dev/nvidiactl:/dev/nvidiactl \
+          --device=/dev/nvidia-uvm:/dev/nvidia-uvm"
+VDIR="-v ${LWDIR}:${WDIR}"
+DOCK_OPT="${DOCK_NVD} ${VDIR}"
 
 #TYPE is either -g for GPU or -p NN for CPU NN processes
 TAG_TYPE="GPU"
@@ -60,13 +66,17 @@ PWRFIT_PAR="-a ${ANG} -l"
 INPUT_DIR=${WDIR}/${CASEDIR}
 PDB=${INPUT_DIR}/${PDBF}
 MAP=${INPUT_DIR}/${MAPF}
-RESOUT=${WDIR}/${MACH}/res-${CASE}
-TIMEOUT=${WDIR}/${MACH}/time-${CASE}
+RESOUT=${WDIR}/${DOCK_NAME}/res-docker-${CASE}
+LRESOUT=${LWDIR}/${DOCK_NAME}/res-docker-${CASE}
+TIMEOUT=${LWDIR}/${DOCK_NAME}/time-docker-${CASE}
 
-mkdir -p ${RESOUT}
+mkdir -p ${LRESOUT}
 mkdir -p ${TIMEOUT}
 
 echo "-> Input files: ${PDB} ${MAP} Resolution: ${RESOL}"
+
+### Run on the GPUs
+DOCK_RUN="docker run ${DOCK_OPT} ${DOCK_NAME}"
 
 for i in `seq -w ${NRUNS}`
 do
@@ -80,8 +90,7 @@ do
   echo "-> Run num: ${i}"
   echo
   echo "-> Executing:"
-  echo     "${TIME} -f ${TIME_STR} -o ${TIME_RES} powerfit ${MAP} ${RESOL} ${PDB} ${PWRFIT_PAR} ${TYPE} -d ${OUT_DIR}"
+  echo     "${TIME} -f ${TIME_STR} -o ${TIME_RES} ${DOCK_RUN} disvis ${PDB1} ${PDB2} ${REST} ${DISVIS_PAR} ${TYPE} -d ${OUT_DIR}"
   echo
-  ${TIME} -f "${TIME_STR}" -o ${TIME_RES} powerfit ${MAP} ${RESOL} ${PDB} ${PWRFIT_PAR} ${TYPE} -d ${OUT_DIR}
+  ${TIME} -f "${TIME_STR}" -o ${TIME_RES} ${DOCK_RUN} powerfit ${MAP} ${RESOL} ${PDB} ${PWRFIT_PAR} ${TYPE} -d ${OUT_DIR}
 done
-
