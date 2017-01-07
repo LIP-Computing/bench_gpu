964c6c65c593027f508e4d70ce24865baf4b04e8
mariojmdavid <mariojmdavid@gmail.com>
2017-01-07 22:44:27 +0000

    new python analisys script

---
+++ analisys/analisys-2.py
@@ -0,0 +1,76 @@
+#########################################################################
+# Developed in the framework of INDIGO project,
+#
+# Author: Mario David <mariojmdavid@gmail.com>
+#########################################################################
+
+import os
+import pprint
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    # TODO; this is input arg
+    root_dir = "/home/david/Dropbox/AA-work/bench-gpu-results"
+
+    machines = ['docker-disvis-centos7-build1',
+                'docker-disvis-ubuntu16.04-build1',
+                'Phys-Mach',
+                'udocker-disvis-ubuntu16.04-build1']
+    cases = ['RNA-polymerase-II', 'PRE5-PUP2-complex']
+    angles = ['5.0', '10.0']
+    voxspac = ['1', '2']
+    types = ['GPU']
+    lrunsG = ['1', '2', '3', '4', '5']
+    ljsres = []
+    laggrRes = []
+
+    i = 0 # index of machine type matches the index of total number of cores
+    for mach in machines:
+        if mach != 'Phys-Mach':
+            initName = 'time-docker-'
+        else:
+            initName = 'time-'
+        for cs in cases:
+            for ang in angles:
+                for vs in voxspac:
+                    aggrRes = {'machine': mach, 'case': cs, 'angle': ang,
+                               'voxspac': vs,
+                               'GPU': {'mean': None, 'stdev': None}}
+                    laggrRes.append(aggrRes)
+                    jsres = {'machine': mach, 'case': cs, 'angle': ang,
+                             'voxspac': vs, 'type': 'GPU', 'rtime': []}
+                    for n in lrunsG:
+                        tr_file = root_dir + os.sep + mach + os.sep + \
+                            initName + cs + os.sep + 'tr_ang-' + ang + \
+                            '-vs-' + vs + '-type-GPU' + '-n-' + n + '.txt'
+                        try:
+                            f = open(tr_file)
+                            #print '-> File: %s' % tr_file
+                            s = f.readline()
+                            rtime = float(s)
+                            jsres['rtime'].append(rtime)
+                        except IOError:
+                            print 'Cannot open', tr_file
+                    ljsres.append(jsres)
+        i += 1
+
+    for jsres in ljsres:
+        jsres['rtime'] = np.array(jsres['rtime'])
+        jsres['mean'] = np.mean(jsres['rtime'])
+        jsres['stdev'] = np.std(jsres['rtime'])
+        print jsres['case'], jsres['machine'], jsres['angle'], jsres['voxspac'], jsres['type']
+
+    for j in ljsres:
+        for a in laggrRes:
+            if j['case'] == a['case'] and j['machine'] == a['machine'] and \
+               j['angle'] == a['angle'] and j['voxspac'] == a['voxspac']:
+                if j['type'] == 'GPU':
+                    a['GPU']['mean'] = j['mean']
+                    a['GPU']['stdev'] = j['stdev']
+
+    print '--------------------------------------'
+    print 'Case\tMachine\tAngle\tVoxSpac\tTimeGPU (sec)\tSTDev (sec)'
+    for a in laggrRes:
+        print '%s\t%s\t%s\t%s\t%5.1f\t%5.1f' % \
+            (a['case'], a['machine'], a['angle'], a['voxspac'], a['GPU']['mean'], a['GPU']['stdev'])
