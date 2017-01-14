30c933fdadfd822af1816b4207a05ed8efdacd7a
mariojmdavid <mariojmdavid@gmail.com>
2017-01-14 18:57:49 +0000

    add analisys of powerfit cases

--- analisys/analisys-2.py
+++ analisys/analisys-2.py
@@ -14,7 +14,11 @@ if __name__ == '__main__':
 
     machines = ['Phys-C7-QK2200',
                 'Phys-C7-QK5200',
-                'VM-U16-TK40']
+                'VM-U16-TK40',
+                'Dock-C7-QK5200',
+                'Dock-C7-TK40',
+                'Dock-U16-QK5200',
+                'Dock-U16-TK40']
     cases = ['RNA-polymerase-II', 'PRE5-PUP2-complex']
     angles = ['5.0', '10.0']
     voxspac = ['1', '2']
@@ -41,7 +45,7 @@ if __name__ == '__main__':
                             '-vs-' + vs + '-type-GPU' + '-n-' + n + '.txt'
                         try:
                             f = open(tr_file)
-                            print '-> File: %s' % tr_file
+                            #print '-> File: %s' % tr_file
                             s = f.readline()
                             rtime = float(s)
                             jsres['rtime'].append(rtime)
@@ -69,3 +73,51 @@ if __name__ == '__main__':
     for a in laggrRes:
         print '%s\t%s\t%s\t%s\t%5.1f\t%5.1f' % \
             (a['case'], a['machine'], a['angle'], a['voxspac'], a['GPU']['mean'], a['GPU']['stdev'])
+
+    print '--------------------------------------'
+    print '--------------------------------------'
+    cases = ['GroEL-GroES', 'RsgA-ribosome']
+    lrunsG = ['1', '2', '3', '4', '5']
+    ljsres = []
+    laggrRes = []
+    i = 0  # index of machine type matches the index of total number of cores
+    for mach in machines:
+        initName = 'time-'
+        for cs in cases:
+            aggrRes = {'machine': mach, 'case': cs, 'GPU': {'mean': None, 'stdev': None}}
+            laggrRes.append(aggrRes)
+            jsres = {'machine': mach, 'case': cs, 'type': 'GPU', 'rtime': []}
+            for n in lrunsG:
+                tr_file = root_dir + os.sep + mach + os.sep + initName + cs + os.sep + 'tr_ang-4.71' + \
+                          '-type-GPU' + '-n-' + n + '.txt'
+                try:
+                    f = open(tr_file)
+                    # print '-> File: %s' % tr_file
+                    s = f.readline()
+                    rtime = float(s)
+                    jsres['rtime'].append(rtime)
+                except IOError:
+                    print 'Cannot open', tr_file
+            ljsres.append(jsres)
+        i += 1
+    
+    for jsres in ljsres:
+        jsres['rtime'] = np.array(jsres['rtime'])
+        jsres['mean'] = np.mean(jsres['rtime'])
+        jsres['stdev'] = np.std(jsres['rtime'])
+        print jsres['case'], jsres['machine'], jsres['type']
+    
+    for j in ljsres:
+        for a in laggrRes:
+            if j['case'] == a['case'] and j['machine'] == a['machine']:
+                if j['type'] == 'GPU':
+                    a['GPU']['mean'] = j['mean']
+                    a['GPU']['stdev'] = j['stdev']
+    
+    print '--------------------------------------'
+    print 'Case\tMachine\tTimeGPU (sec)\tSTDev (sec)'
+    for a in laggrRes:
+        print '%s\t%s\t%5.1f\t%5.1f' % \
+              (a['case'], a['machine'], a['GPU']['mean'], a['GPU']['stdev'])
+
+
