d8c6ad05bdfa8bc83b35042e6d8fe0a47f6c40b1
mariojmdavid <mariojmdavid@gmail.com>
2017-03-05 16:28:10 +0000

    implementation phase

--- analisys/mat-plot.py
+++ analisys/mat-plot.py
@@ -8,23 +8,30 @@ import os
 import pprint
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 if __name__ == '__main__':
     # TODO; this is input arg
     csvfile = "/home/david/Dropbox/AA-work/bench-run4/bench-results01-4th-bench.csv"
 
-    machPh = ['Phys-C7-QK5200',
-              'Dock-C7-QK5200',
-              'Dock-U16-QK5200',
-              'UDock-C7-QK5200',
-              'UDock-U16-QK5200'
-              ]
-    machVM = ['VM-U16-TK40',
-              'Dock-C7-TK40',
-              'Dock-U16-TK40',
-              'UDock-C7-TK40',
-              'UDock-U16-TK40'
-              ]
-    cases = ['RNA-polymerase-II', 'PRE5-PUP2-complex']
-    angles = ['5.0', '10.0']
-    voxspac = ['1', '2']
+    csvhead = ['Case', 'Machine', 'Time', 'STDev']
+    machines = ['Phys-C7-QK5200',
+                'Dock-C7-QK5200',
+                'Dock-U16-QK5200',
+                'UDock-C7-QK5200',
+                'UDock-U16-QK5200',
+                'VM-U16-TK40',
+                'Dock-C7-TK40',
+                'Dock-U16-TK40',
+                'UDock-C7-TK40',
+                'UDock-U16-TK40']
+    
+    cases = ['RNA-polymerase-II Angle = 5 VoxSpac = 1',
+             'PRE5-PUP2-complex Angle = 5 VoxSpac = 1',
+             'GroEL-GroES Angle = 4.71 resolution = 23',
+             'RsgA-ribosome Angle = 4.71 resolution = 13.3']
+    
+    with open(csvfile, 'rb') as cf:
+        rowdata = csv.DictReader(cf)
+        for row in rowdata:
+            print(row['Case'], row['Machine'])
